8c74383a2290018f33377a9c7482af252a0faca6
MURARI <mogalimurari29@gmail.com>
2024-08-22 15:46:26 +0530

    c1

---
+++ insurance_check.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vehicle Insurance Checker</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>Vehicle Insurance Checker</h1>
+	<form id="insurance-form">
+		<label for="vehicle-no">Enter Vehicle Number:</label>
+		<input type="text" id="vehicle-no" name="vehicle-no">
+		<button type="submit">Check Insurance</button>
+	</form>
+	<div id="insurance-data"></div>
+
+	<script src="script.js"></script>
+</body>
+</html>
